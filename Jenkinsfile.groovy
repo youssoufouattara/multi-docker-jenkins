0717508efa9ea8b430589480fd8f1f4cd23d284d
youatt <ouattarayoussouf807@gmail.com>
2023-12-15 14:57:49 +0100

    correct jenkinsfile3

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,30 +1,5 @@
 def project_token = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEF'
 
-properties([
-    githubConnection('your--connection-name'),
-    pipelineTriggers([
-        [
-            $class: 'GitLabPushTrigger',
-            branchFilterType: 'All',
-            triggerOnPush: true,
-            triggerOnMergeRequest: true,
-            triggerOpenMergeRequestOnPush: "never",
-            triggerOnNoteRequest: true,
-            noteRegex: "Jenkins please retry a build",
-            skipWorkInProgressMergeRequest: true,
-            secretToken: project_token,
-            ciSkip: false,
-            setBuildDescription: true,
-            addNoteOnMergeRequest: true,
-            addCiMessage: true,
-            addVoteOnMergeRequest: true,
-            acceptMergeRequestOnSuccess: true,
-            branchFilterType: "NameBasedFilter",
-            includeBranchesSpec: "",
-            excludeBranchesSpec: "",
-        ]
-    ])
-])
 
 node(){
   try{
